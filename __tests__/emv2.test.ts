371f2f0ef12fafaf0f8426d8b052c4360c072aba
AJ Larson <aj@devmounta.in>
2020-12-14 23:28:28 -0700

    updating with the solutions for requirements 1 & 2

--- __tests__/emv2.test.ts
+++ __tests__/emv2.test.ts
@@ -1,33 +1,10 @@
 import { EmployeeManager, Employee } from "./pageObjects/EmployeeManager";
 
-const employees: Array<Employee> = [
-  {
-    name: "Han Solo",
-    phone: 1111111111,
-    email: "millenium@falcon.rep",
-    title: "Smuggler",
-  },
-  {
-    name: "Luke Skywalker",
-    phone: 2222222222,
-    email: "red5@rogue.rep",
-    title: "Jedi",
-  },
-  {
-    name: "Thrawn",
-    phone: 3333333333,
-    email: "gathrawn@admiralty.emp",
-    title: "Best Villain",
-  },
-  {
-    name: "R2-D2",
-    phone: 4444444444,
-    email: "pottymouth@astromech.rep",
-    title: "Crotchety Old Droid",
-  },
-];
+// you don't need to set the JSON in a new folder, but I thought it was a good example
+import * as employees from "./data/employees.json";
 
 describe("employee manager v2", () => {
+  // this is where you'd want to change firefox vs chrome to update your testing.
   const page = new EmployeeManager({ browser: "chrome" });
   beforeEach(async () => {
     await page.navigate();
@@ -41,21 +18,28 @@ describe("employee manager v2", () => {
     let resultList = await page.getEmployeeList();
     expect(originalList.length).toBeGreaterThanOrEqual(resultList.length);
   });
-  test("Can add and delete an employee", async () => {
-    let newEmployee = {
-      name: "Test Employee",
-      phone: 1234567890,
-      email: "test@email.com",
-      title: "test person",
-    };
-    await page.addEmployee(newEmployee);
-    let employee = await page.getCurrentEmployee();
-    expect(employee.name).toEqual(newEmployee.name);
-    expect(employee.phone).toEqual(newEmployee.phone);
-    expect(employee.email).toEqual(newEmployee.email);
-    expect(employee.title).toEqual(newEmployee.title);
-    await page.deleteEmployee("Test Employee");
-    let employeeList = await page.getEmployeeList();
-    expect(employeeList).not.toContain("Test Employee");
+  test("Screenshotting the 'screenshot' employees", async () => {
+    // we need to search for the right group of employees
+    await page.searchFor("Screenshot");
+    // and then we can take the screenshot -- though I did have to make the folder "screenshots" manually first
+    await page.takeScreenshot("screenshots/screenshot");
+  });
+  //I literally wrapped the old add/delete test in a loop, and updated a few parameters.
+  //That's it.
+  employees.forEach((newEmployee) => {
+    //I updated the test name
+    test(`Can add and delete an employee (newEmployee.name)`, async () => {
+      await page.addEmployee(newEmployee);
+      let employee = await page.getCurrentEmployee();
+      expect(employee.name).toEqual(newEmployee.name);
+      expect(employee.phone).toEqual(newEmployee.phone);
+      expect(employee.email).toEqual(newEmployee.email);
+      expect(employee.title).toEqual(newEmployee.title);
+      //had to update this argument
+      await page.deleteEmployee(newEmployee.name);
+      let employeeList = await page.getEmployeeList();
+      //and this one
+      expect(employeeList).not.toContain(newEmployee.name);
+    });
   });
 });
